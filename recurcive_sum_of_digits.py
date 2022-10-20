e48ef057318226aab98302d4269a3815dfef9097
Xenokrat <ksenokrat@gmail.com>
2022-10-20 21:20:53 +0300

    fix 3: remove no needed variables

--- recurcive_sum_of_digits.py
+++ recurcive_sum_of_digits.py
@@ -1,7 +1,4 @@
 def calc_recr_sum_of_digits(number: int) -> int:
     if number < 10:
         return number
-
-    last_digit = number % 10
-    number_no_last_digit = number // 10
-    return last_digit + calc_recr_sum_of_digits(number_no_last_digit)
+    return number % 10 + calc_recr_sum_of_digits(number // 10)
