3d48e68b3125008a5c3ecc4256aed1a11d6b9209
Xenokrat <ksenokrat@gmail.com>
2022-10-21 09:30:04 +0300

    fix 4: change out-of-recr check to less-equal

--- is_palindrome_recr.py
+++ is_palindrome_recr.py
@@ -1,6 +1,6 @@
 def is_palindrome_recr(text: str):
     """Проверка является ли текст палиндромом. Без учета регистра букв, с учетом пробелов"""
-    if len(text) == 1:
+    if len(text) <= 1:
         return True
     if text[0].lower() != text[-1].lower():
         return False
