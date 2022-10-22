902afd18c3d3421c1bd171a045f23aa02ee390c9
Mantis <barabanovpv95@gmail.com>
2022-10-22 13:21:41 +0300

    fix 5: no slicing ^| copy ^| change original list

--- print_even_numbers.py
+++ print_even_numbers.py
@@ -1,17 +1,17 @@
 from typing import List
 
 
-def print_even_numbers(arr: List[int]):
-    arr_copy = arr[:]
-    return print_even_numbers_recr(arr_copy)
+def print_even_numbers(arr: List[int]) -> None:
+    arr_length: int = len(arr)
+    return print_even_numbers_recr(arr, 0, arr_length - 1)
 
 
-def print_even_numbers_recr(arr_copy: List[int]):
-    if not arr_copy:
+def print_even_numbers_recr(arr: list, index: int, arr_length: int) -> None:
+    if arr[index] % 2 == 0:
+        print(arr[index], end=' ')
+
+    if index >= arr_length:
         return None
 
-    element: int = arr_copy.pop(0)
-    if element % 2 == 0:
-        print(element, end=' ')
-    return print_even_numbers(arr_copy)
+    return print_even_numbers_recr(arr, index + 1, arr_length)
 
