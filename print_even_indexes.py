b66b8eaf0a338626ce6021be1ee257dcda6301c7
Mantis <barabanovpv95@gmail.com>
2022-10-22 18:08:41 +0300

    fix 5, 6: check if index less than arr_length instead

--- print_even_indexes.py
+++ print_even_indexes.py
@@ -1,14 +1,12 @@
 def print_even_indexes(arr: list) -> None:
     arr_length: int = len(arr)
-    return print_even_indexes_recr(arr, 0, arr_length - 1)
+    return print_even_indexes_recr(arr, 0, arr_length)
 
 
 def print_even_indexes_recr(arr: list, index: int, arr_length: int) -> None:
-    if index % 2 == 0:
-        print(arr[index], end=' ')
+    if index < arr_length:
+        if index % 2 == 0:
+            print(arr[index], end=' ')
 
-    if index >= arr_length:
-        return None
-
-    return print_even_indexes_recr(arr, index + 1, arr_length)
+        return print_even_indexes_recr(arr, index + 1, arr_length)
 
--- print_even_numbers.py
+++ print_even_numbers.py
@@ -3,15 +3,12 @@ from typing import List
 
 def print_even_numbers(arr: List[int]) -> None:
     arr_length: int = len(arr)
-    return print_even_numbers_recr(arr, 0, arr_length - 1)
+    return print_even_numbers_recr(arr, 0, arr_length)
 
 
 def print_even_numbers_recr(arr: List[int], index: int, arr_length: int) -> None:
-    if arr[index] % 2 == 0:
-        print(arr[index], end=' ')
+    if index < arr_length:
+        if arr[index] % 2 == 0:
+            print(arr[index], end=' ')
 
-    if index >= arr_length:
-        return None
-
-    return print_even_numbers_recr(arr, index + 1, arr_length)
-
+        return print_even_numbers_recr(arr, index + 1, arr_length)
