febd65e6cf744c5e8c7520fa266c16706fffa7b9
Mantis <barabanovpv95@gmail.com>
2022-10-22 13:22:48 +0300

    fix 5: fix annotations

--- print_even_numbers.py
+++ print_even_numbers.py
@@ -6,7 +6,7 @@ def print_even_numbers(arr: List[int]) -> None:
     return print_even_numbers_recr(arr, 0, arr_length - 1)
 
 
-def print_even_numbers_recr(arr: list, index: int, arr_length: int) -> None:
+def print_even_numbers_recr(arr: List[int], index: int, arr_length: int) -> None:
     if arr[index] % 2 == 0:
         print(arr[index], end=' ')
 
