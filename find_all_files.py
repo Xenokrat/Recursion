baa10f41523ed84e40cdc64d8b47101856eb2995
Mantis <barabanovpv95@gmail.com>
2022-10-24 18:20:15 +0300

    fix 8: change function to return list and not touch paramters

--- find_all_files.py
+++ find_all_files.py
@@ -1,25 +1,22 @@
 from os import listdir
-from os.path import isdir, join
-from typing import List
+from os.path import isdir, join, isfile
 
 
-def find_all_files(path: str) -> List[str]:
-    files = []
-    find_all_files_recr(path, files)
+def find_all_files(path: str):
+    dirs_and_files = listdir(path)
+
+    dirs = [
+        item for item in dirs_and_files
+        if isdir(join(path, item))
+    ]
+
+    files = [
+        item for item in dirs_and_files
+        if isfile(join(path, item))
+    ]
+
+    for item in dirs:
+        files.extend(find_all_files(join(path, item)))
+
     return files
 
-
-def find_all_files_recr(path: str, files: List[str]) -> None:
-    dirs_and_files = listdir(path)
-    dirs_here = []
-
-    for item in dirs_and_files:
-        if isdir(join(path, item)):
-            dirs_here.append(item)
-        else:
-            files.append(item)
-
-    for item in dirs_here:
-        new_path: str = join(path, item)
-        find_all_files_recr(new_path, files)
-        
