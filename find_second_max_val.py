38b972f91143684d3826aa58b2ea55aa4bfb0ae1
Mantis <barabanovpv95@gmail.com>
2022-10-24 12:15:17 +0300

    fix 7: change recursive call to operate by index

--- find_second_max_val.py
+++ find_second_max_val.py
@@ -1,27 +1,30 @@
-def find_second_max_val(arr: list):
-    # считаем, что у списка изначально нет second_max
-    # если он из одного элемента
+from typing import List
+
+
+def find_second_max_val(arr: List):
+    # считаем, что у списка изначально нет second_max если он из одного элемента
     if len(arr) < 2:
         return None
 
     first_max = arr[0]
     second_max = float('-inf')
 
-    return find_second_max_recr(arr[1:], first_max, second_max)
+    return find_second_max_recr(arr, 1, first_max, second_max)
 
 
-def find_second_max_recr(arr, first_max, second_max):
+def find_second_max_recr(arr: List, index: int, first_max, second_max):
 
-    value = arr[0]
+    if index >= len(arr):
+        return second_max
+
+    value = arr[index]
 
     if value > first_max:
         second_max = first_max
         first_max = value
+
     elif value > second_max:
         second_max = value
 
-    if len(arr) < 2:
-        return second_max
-
-    return find_second_max_recr(arr[1:], first_max, second_max)
+    return find_second_max_recr(arr, index + 1, first_max, second_max)
 
