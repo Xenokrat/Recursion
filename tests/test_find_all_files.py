d05a5d2205c37bc0f3d2cd5af54390d28e019695
Mantis <barabanovpv95@gmail.com>
2022-10-24 12:16:04 +0300

    fix 8: tested on windows path

--- tests/test_find_all_files.py
+++ tests/test_find_all_files.py
@@ -5,7 +5,7 @@ from find_all_files import find_all_files
 
 class MyTestCase(unittest.TestCase):
     def test_find_all_files(self) -> None:
-        files = find_all_files('~/test_delete')
+        files = find_all_files(r'C:\Users\Mantis\test_delete')
         self.assertListEqual(
             files, [
                 'test.txt',
